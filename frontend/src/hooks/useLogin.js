6e14aab81447138a731b48e991d04487018831d3
vaibhav kamade <vaibhavkamade20@gmail.com>
2024-08-13 23:57:20 +0530

    server1 added

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -9,7 +9,7 @@ export const useLogin = () =>{
     const login = async (email,password) =>{
         setIsLoading(true);
         setError(null);
-        const response = await fetch('/api/user/login',{
+        const response = await fetch('https://workout-app-dxm8.onrender.com/api/user/login',{
             method:'POST',
             headers:{'Content-Type':'application/json'},
             body:JSON.stringify({email,password})

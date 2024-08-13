47bf01e6b23cf85fe319225ae1d939cd9840e1d9
vaibhav kamade <vaibhavkamade20@gmail.com>
2024-08-14 00:02:01 +0530

    server2 added

--- backend/server.js
+++ backend/server.js
@@ -4,37 +4,38 @@ const express = require('express');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workoutRoutes');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');  // Import CORS
 
-//express app
+// express app
 const app = express();
 
+// CORS configuration
+const corsOptions = {
+    origin: ['https://workout-app-three-navy.vercel.app', 'http://localhost:3000'],  // Include your frontend's URL here
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,  // Allow cookies and headers
+};
 
+app.use(cors(corsOptions));  // Apply CORS
 
 app.use(express.json());
-app.use((req,res,next)=>{
-    console.log(req.path , req.method);
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
     next();
-})
+});
 
+// routes
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
-//routes
-
-app.use('/api/workouts',workoutRoutes);
-app.use('/api/user',userRoutes);
-
-
-//connection to database
-
+// connection to database
 mongoose.connect(process.env.MONGODB_URI)
-    .then(()=>{
-        //listening on port
-        app.listen(process.env.PORT,(req,res)=>{
-            console.log("connected on port " , process.env.PORT);
+    .then(() => {
+        // listening on port
+        app.listen(process.env.PORT, () => {
+            console.log("connected on port ", process.env.PORT);
         });
     })
-    .catch((err)=>{
-        console.log(err)
-    })
-
-
-
+    .catch((err) => {
+        console.log(err);
+    });
